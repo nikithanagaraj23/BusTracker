d54add8990a79dbaf53f76c52353ad0f5bd36db3
chandhinichandersekar <chandersekar.c@husky.neu.edu>
2018-04-19 20:37:57 -0400

    Update prod.secret.exs

--- bustracker/config/prod.secret.exs
+++ bustracker/config/prod.secret.exs
@@ -8,11 +8,11 @@ use Mix.Config
 # file or create a script for recreating it, since it's
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
-config :tasktrackerspa, BustrackerWeb.Endpoint,
+config :bustracker, BustrackerWeb.Endpoint,
   secret_key_base: "6fTJ5iaRQDZ6c6DyUR65RSolH6eW28HQjxBGjKpMfPDctN/UU/l6V73mLHdreA3c"
 
 # Configure your database
-config :tasktrackerspa, Bustracker.Repo,
+config :bustracker, Bustracker.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "bustracker",
   password: "bustracker",
